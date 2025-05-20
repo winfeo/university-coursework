5e18bdc1077b07658f369d8567c8031a175344e0
Kirill <kirill.ryabikin.05@gmail.com>
2025-05-20 14:13:29 +0300

    Установка кастомной иконки приложения

--- app/src/main/java/com/example/university_coursework/fragments/HomeFragment.java
+++ app/src/main/java/com/example/university_coursework/fragments/HomeFragment.java
@@ -21,18 +21,41 @@ import com.example.university_coursework.PatientCard;
 import com.example.university_coursework.R;
 import com.example.university_coursework.database.*;
 
+/**
+ * Фрагмент главного экрана приложения, отображающий список пациентов врача.
+ * Позволяет переходить к карточке пациента и просматривать уведомления.
+ */
 public class HomeFragment extends Fragment {
     ArrayList<PatientInfo> doctorsPatients = StoreDatabases.getDoctorsPatients();//получаем список пациентов врача
 
+    /**
+     * Создает и возвращает представление фрагмента
+     * @param inflater LayoutInflater для создания представления из XML
+     * @param container Родительский ViewGroup
+     * @param savedInstanceState Сохраненное состояние
+     * @return Корневое представление фрагмента
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    /**
+     * Вызывается после создания представления фрагмента.
+     * Инициализирует RecyclerView с пациентами и другие UI элементы.
+     * @param view Корневое представление фрагмента
+     * @param savedInstanceState Сохраненное состояние
+     */
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        /**
+         * Обрабатывает нажатие на карточку пациента
+         * @param patient Объект пациента
+         * @param position Позиция в списке
+         * @param view Нажатое представление
+         */
         // слушатель нажатия элемента в списке
         PatientMiniCardAdapter.OnStateClickListener stateClickListener = new PatientMiniCardAdapter.OnStateClickListener() {
             @Override
@@ -55,6 +78,10 @@ public class HomeFragment extends Fragment {
         greetingPhrase.setText(getString(R.string.greetings) + " " + DoctorInfo.getObject().getName() + "!");
     }
 
+    /**
+     * Вызывается при возобновлении работы фрагмента.
+     * Настраивает кнопку уведомлений.
+     */
     @Override
     public void onResume() {
         super.onResume();
@@ -62,6 +89,11 @@ public class HomeFragment extends Fragment {
         //Уведомления
         ImageButton button = getView().findViewById(R.id.notificationButton);
         button.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * Обрабатывает нажатие на кнопку уведомлений
+             * @param view Нажатое представление
+             */
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), NotificationList.class);
